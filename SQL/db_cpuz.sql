7a5518054a888086b7c6995056bd2a087b3fd430
Samba SENE <80283818+SambaSENE@users.noreply.github.com>
2024-02-27 08:46:46 +0100

    add

--- SQL/db_cpuz.sql
+++ SQL/db_cpuz.sql
@@ -27,13 +27,14 @@ CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
   PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
--- Listage des données de la table db_cpuz.doctrine_migration_versions : ~2 rows (environ)
+-- Listage des données de la table db_cpuz.doctrine_migration_versions : ~4 rows (environ)
 DELETE FROM `doctrine_migration_versions`;
 INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
 	('DoctrineMigrations\\Version20240219083731', '2024-02-19 08:38:01', 30),
 	('DoctrineMigrations\\Version20240219090413', '2024-02-19 09:04:20', 42),
 	('DoctrineMigrations\\Version20240222093617', '2024-02-22 09:36:27', 63),
-	('DoctrineMigrations\\Version20240222113620', '2024-02-22 11:36:33', 76);
+	('DoctrineMigrations\\Version20240222113620', '2024-02-22 11:36:33', 76),
+	('DoctrineMigrations\\Version20240226100839', '2024-02-26 10:09:12', 70);
 
 -- Listage de la structure de table db_cpuz. microprocesseur
 CREATE TABLE IF NOT EXISTS `microprocesseur` (
@@ -42,22 +43,23 @@ CREATE TABLE IF NOT EXISTS `microprocesseur` (
   `famille` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
   `model` varchar(7) COLLATE utf8mb4_unicode_ci NOT NULL,
   `frequence` decimal(2,1) NOT NULL,
-  `prix` int NOT NULL DEFAULT '0',
+  `prix` decimal(3,0) NOT NULL,
+  `stock` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Listage des données de la table db_cpuz.microprocesseur : ~9 rows (environ)
 DELETE FROM `microprocesseur`;
-INSERT INTO `microprocesseur` (`id`, `marque`, `famille`, `model`, `frequence`, `prix`) VALUES
-	(101, 'AMD', 'Ryzen 3', '3200G', 3.6, 99),
-	(102, 'AMD', 'Ryzen 5', '3600', 3.6, 129),
-	(103, 'AMD', 'Ryzen 5', '5600G', 3.9, 149),
-	(104, 'AMD', 'Ryzen 7', '5800X', 3.8, 249),
-	(105, 'AMD', 'Ryzen 7', '7800X3D', 4.2, 499),
-	(106, 'INTEL', 'Core i5', '13500', 2.5, 139),
-	(107, 'INTEL', 'Core i5', '13600K', 3.5, 159),
-	(108, 'INTEL', 'Core i7', '13700K', 3.4, 169),
-	(109, 'INTEL', 'Core i7', '4790', 3.6, 79);
+INSERT INTO `microprocesseur` (`id`, `marque`, `famille`, `model`, `frequence`, `prix`, `stock`) VALUES
+	(101, 'AMD', 'Ryzen 3', '3200G', 3.6, 99, 0),
+	(102, 'AMD', 'Ryzen 5', '3600', 3.6, 129, 0),
+	(103, 'AMD', 'Ryzen 5', '5600G', 3.9, 149, 0),
+	(104, 'AMD', 'Ryzen 7', '5800X', 3.8, 249, 0),
+	(105, 'AMD', 'Ryzen 7', '7800X3D', 4.2, 499, 0),
+	(106, 'INTEL', 'Core i5', '13500', 2.5, 139, 0),
+	(107, 'INTEL', 'Core i5', '13600K', 3.5, 159, 0),
+	(108, 'INTEL', 'Core i7', '13700K', 3.4, 169, 0),
+	(109, 'INTEL', 'Core i7', '4790', 3.6, 79, 0);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
